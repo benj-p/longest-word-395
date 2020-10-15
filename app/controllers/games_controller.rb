bff4fcc9612722a4163fa22bde9672b5e1251265
Ben Pham <benjamin.pham@gmail.com>
2020-10-15 10:35:16 +0200

    Completed longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,47 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  VOWELS = %w(a e i o u y)
+
+  def new
+    # Generate grid with 5 vowels and 5 consonants, the same letter can be there multiple times
+    @grid = Array.new(5) { VOWELS.sample }
+    @grid += Array.new(5) { (('a'..'z').to_a - VOWELS).sample }
+    @grid.shuffle!
+  end
+
+  def score
+    # Initialize session with 0 points if no score
+    session[:score] = 0 unless session[:score]
+
+    @attempt = params[:attempt].downcase.split(//)
+    @grid = params[:grid].split(' ')
+
+    # Check if letters are included in grid and if they are not used more times than the number of times they appear in the grid
+    @letters_included = included?(@grid, @attempt)
+
+    # Check if word is english
+    @valid_english_word = english_word?(params[:attempt])
+
+    # Calculate score and increment total score
+    @score = update_score(@valid_english_word, @letters_included, params[:attempt])
+  end
+
+  private
+
+  def included?(grid, attempt)
+    attempt.all? { |letter| attempt.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    word_deserialized = open(url).read
+    JSON.parse(word_deserialized)['found']
+  end
+
+  def update_score(valid_english_word, letters_included, attempt)
+    score = valid_english_word && letters_included ? attempt.length : 0
+    session[:score] += score
+    return score
+  end
+end
